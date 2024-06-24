4dc2d935aa9ee10f12838d652f8b15413b18d7c5
renwentao <renwentao@pagsmile.com>
2024-06-24 10:13:27 +0800

    style: 定义row优化页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,56 @@
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 
 export const AuthenticatedApp = () => {
-  const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>logout</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <PageHeader />
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const PageHeader = () => {
+  const { logout } = useAuth();
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h2>logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <button onClick={logout}>logout</button>
+      </HeaderRight>
+    </Header>
+  );
+};
+
+const HeaderItem = styled.h3`
+  margin-right: 3rem;
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  display: flex;
+  overflow: hidden;
+`;
